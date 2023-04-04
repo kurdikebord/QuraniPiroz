c8939e57bfa4dd457e01cc5391f768263f54dd6b
kurdikeyboard <86693256+kurdikebord@users.noreply.github.com>
2023-04-05 01:23:21 +0300

    Translate Topics & Prophets name
    Signed-off-by: kurdikeyboard <86693256+kurdikebord@users.noreply.github.com>

--- app/src/main/java/com/goran/quranipiroz/api/ApiConfig.kt
+++ app/src/main/java/com/goran/quranipiroz/api/ApiConfig.kt
@@ -1,8 +1,8 @@
 package com.goran.quranipiroz.api
 
 object ApiConfig {
-    const val WEBSITE_ROOT_URL = "https://raw.githubusercontent.com/kurdikeyboard/QuraniPiroz/master/"
-    const val QURANI_PIROZ_URL = "https://kurdikeyboard.github.io/quran"
+    const val WEBSITE_ROOT_URL = "https://raw.githubusercontent.com/kurdikebord/QuraniPiroz/master/"
+    const val QURANI_PIROZ_URL = "https://kurdikebord.github.io/quran"
     const val BUG_REPORT_URL = "https://m.me/kurdikeyboard"
     const val AYAT_REPORT_URL =  "mailto:gorangharib.909@gmail.com"
     const val QURAN_API_ROOT_URL = "https://api.quran.com/"
